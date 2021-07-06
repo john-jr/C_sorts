edd7734064cb296b953731a54ae7158f7c97fd3c
john-jr <outjohn@outlook.com>
2021-07-06 12:26:35 -0300

    Teste

--- Bubble_Sort.c
+++ Bubble_Sort.c
@@ -5,7 +5,8 @@ int * createArray(int *, int);
 void bubbleSortArray(int *, int tam_array);
 
 void WinMain(){
-   int tam_array;
+   int tam_array;\
+   int *p; 
       printf("Digite a quantidade de elementos do vetor: ");
       scanf("%d",&tam_array);
    bubbleSortArray(createArray(&p, tam_array), tam_array);
