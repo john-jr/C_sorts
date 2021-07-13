e14dc6448e531daff0bddcad8fe1154090c2a96c
Joao Bosco <outjohn@outlook.com>
2021-07-13 17:16:56 -0300

    13/07 - 17:17

--- Module1-3/Exercice2/Dado.c
+++ Module1-3/Exercice2/Dado.c
@@ -1,41 +1,57 @@
+#include "../../Module1-3/Errors.c";
+#include "../../Module1-3/Exercice2/Arvore.c";
+
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <math.h>
 #include <stdbool.h>
+#include <math.h>
 
-#include "../../Module1-3/Errors.c";
+struct dado_satelite
+{
+    int dado_interno;
+    dado * proximo_no_esquerda;
+    dado * proxino_no_direta;
+};
+typedef struct dado_satelite dado;
+
 
 bool isDadoCreated(dado*);
 bool findDado(dado*,int);
-
-typedef struct dado_satelite{
-    int dado_interno;
-    dado * proximo_no_esquerda;
-    dado * proxino_na_direta;
-} dado;
 
 struct dado * createNewDado(int dado_interno){
     dado * Novodado = malloc(sizeof(dado)); 
     if(isDadoCreated(Novodado) == true){
         Novodado->dado_interno = dado_interno;
+    }
 
-}
+};
 
 bool isDadoCreated(dado * dado){
     if(dado == NULL){
         errorMessages(1);
     }
     else{
-        return true
+        return true;
     }
 }
 
-struct bool findDado(dado* dado, int valor_pesquisado){
+bool findDado(dado* dado, int valor_pesquisado){
     if(dado == NULL){
-
+        return false;
     }
-    if(dado)
+    if(dado->dado_interno == valor_pesquisado){
+        return true;
+    }
+
+      if (valor_pesquisado < dado->dado_interno)
+    {
+        return findDado4(dado->proximo_no_esquerda, valor_pesquisado);
+    }
+    else
+    {
+        return findDado4(dado->proxino_no_direta, valor_pesquisado);
+    }
 }
 
 
